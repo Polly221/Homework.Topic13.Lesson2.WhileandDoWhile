6d38ce64ab32745b4cef33fa950840550b3c7a24
Polinka <Polinka@DESKTOP-MHBJDTO>
2023-05-24 23:40:02 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿//ДЗ Тема 13. Урок 2
+//1.
+Random random = new Random();
+int n;
+do
+{
+    n = random.Next(1, 11);
+    Console.WriteLine(n + " ");
+}
+while (n != 7);
+
+//2.
+int d;
+int s = 0;
+do
+{
+    d =int.Parse(Console.ReadLine());
+    s += d;
+}
+while (d != 0);
+Console.WriteLine("Сумма вводимых чисел: " +s);
